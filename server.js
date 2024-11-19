aefeb8f690ed1c4ae48281b5c6870e4a238b577a
thiateleu <thiateleu@gmail.com>
2024-11-19 00:24:01 -0500

    updated

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
